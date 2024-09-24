a8c3439b484c09b908e96d45fe03c45d1feffea3
Bindu Kumari <142012740+SinhaBindu@users.noreply.github.com>
2024-09-24 11:02:00 +0530

    Committed

--- Controllers/UserProfileController.cs
+++ Controllers/UserProfileController.cs
@@ -17,7 +17,7 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.UI.WebControls;
 using System.Xml.Linq;
-using static Bodh.Models.Enums;
+using static Hunarmis.Manager.Enums;
 using Run = DocumentFormat.OpenXml.Spreadsheet.Run;
 using Text = DocumentFormat.OpenXml.Wordprocessing.Text;
 //using DocumentFormat.OpenXml.Packaging;
