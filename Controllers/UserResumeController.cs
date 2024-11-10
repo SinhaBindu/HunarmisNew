772e28d28f617a8d0af89d933e07bb0b710aca8f
Bindu Kumari <142012740+SinhaBindu@users.noreply.github.com>
2024-11-10 08:08:50 +0530

    Committed

---
+++ Controllers/UserResumeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Hunarmis.Controllers
+{
+    public class UserResumeController : Controller
+    {
+        // GET: UserResume
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
